37cc4450b314ce0f936fe4af25f69afb101ffcac
GuFariaszx <zacharingustavo@gmail.com>
2024-06-05 17:23:42 -0300

    feat: Cena history Feita, e Começo da gamificacao

--- src/resources.ts
+++ src/resources.ts
@@ -1,10 +1,12 @@
 import { ImageSource, Loader } from "excalibur";
 import sword from "./images/sword.png";
 import logo from "./images/logo.png"
+import logoVer from "./images/logoVer.png"
 
 export const Resources = {
   Sword: new ImageSource(sword),
-  Logo: new ImageSource(logo)
+  Logo: new ImageSource(logo),
+  logoVer: new ImageSource(logoVer)
 } as const;
 
 export const loader = new Loader();
