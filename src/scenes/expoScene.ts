5d3336fd1fca07b3d9ac22f3c155043eaf3405e2
GuFariaszx <zacharingustavo@gmail.com>
2024-06-11 17:26:32 -0300

    feat: Implementação do Player e configurações  do mesmo

--- src/scenes/expoScene.ts
+++ src/scenes/expoScene.ts
@@ -1,5 +1,6 @@
-import { Color, Engine, FadeInOut, Scene, Transition } from "excalibur";
+import { Color, Engine, FadeInOut, Scene, Transition, vec } from "excalibur";
 import { Resources } from "../resources";
+import { Player } from "../actors/player";
 
 export class expoScene extends Scene {
     onTransition(direction: "in" | "out"): Transition | undefined {
@@ -11,11 +12,31 @@ export class expoScene extends Scene {
     }
 
     onInitialize(engine: Engine<any>): void {
+        this.backgroundColor = Color.Gray
+
         // Carrega o mapa
         let tiledMap = Resources.Mapa
 
+        // Definir offset para renderização do mapa
+        let offsetX = 138
+        let offsetY = 100
+
         // Adicionar o mapa na cena
-        tiledMap.addToScene(this)
+        tiledMap.addToScene(this, {
+            pos: vec(offsetX, offsetY),
+        })
+
+        // Definir zoom da cmaera para aumentar um pouco a vizualização
+        this.camera.zoom = 1.3
+
+        // Criação e configuração do Player
+        let jogador = new Player()
+
+        // Define z-index do player, útil se algum outro elemento ficar "por cima" do jogador
+        jogador.z = 1
+
+        // Adicionar o player na cena
+        this.add(jogador)
     }
 
 }
