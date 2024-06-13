e0029e2305e1d83aaff2a3d7e2411223f5256984
GuFariaszx <zacharingustavo@gmail.com>
2024-06-13 17:22:55 -0300

    feat: Adicionando Sprite e animações

--- src/resources.ts
+++ src/resources.ts
@@ -1,4 +1,4 @@
-import { ImageSource, Loader } from "excalibur";
+import { ImageFiltering, ImageSource, Loader } from "excalibur";
 import { TiledResource } from "@excaliburjs/plugin-tiled";
 
 import sword from "./images/sword.png";
@@ -15,9 +15,12 @@ import tsxBibliotecaPath from "./maps/tileset_biblioteca.tsx?url"
 
 import tmxMapaPath from "./maps/Showroom_map.tmx?url"
 
+import playerSpritePath from "./sprites/Player.png"
+
 export const Resources = {
   Sword: new ImageSource(sword),
   Logo: new ImageSource(logo),
+  PlayerSpriteSheet: new ImageSource(playerSpritePath, { filtering: ImageFiltering.Pixel }),
   logoVer: new ImageSource(logoVer),
   controle: new ImageSource(controle),
   Mapa: new TiledResource(tmxMapaPath, {
